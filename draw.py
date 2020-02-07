990c6119c0345a11bedcf4f826361b6f168f7467
Michael Zhang <mzhang00@stuy.edu>
2020-02-07 11:45:09 -0500

    finish quad 1 yaygit add -Agit add -A

--- draw.py
+++ draw.py
@@ -1,15 +1,18 @@
 from display import *
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-    x = x0
-    y = y0
+def draw_line( x0, y0, x1, y1, screen, color):
+    if x0 > x1:
+        x0, x1 = x1, x0
+        y0, y1 = y1, y0
+    x = int(x0)
+    y = int(y0)
     A = y1 - y0
-    # FIX THIRD LINE NOT SHOWING
     B = x1 - x0
-    m = A/B
-    # FIX POTENTIAL DIVISION BY ZERO ERRORS HERE
-    if (m > 0 and m <= 1):
-    # if (A > 0 and A <= B):
+    #print(x,y)
+    if (A > 0 and A <= B):
+        # print("------------------------------")
+        # print(x,y)
+        # print("------------------------------")
         B *= -1
         d = 2 * A + B
         while x <= x1:
