66bb5783b20cfa92aead30aede68000878e4790c
Michael Zhang <mzhang00@stuy.edu>
2020-02-10 08:33:12 -0500

    add stuff

--- draw.py
+++ draw.py
@@ -8,11 +8,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
     y = int(y0)
     A = y1 - y0
     B = x1 - x0
-    #print(x,y)
     if (A > 0 and A <= B):
-        # print("------------------------------")
-        # print(x,y)
-        # print("------------------------------")
         B *= -1
         d = 2 * A + B
         while x <= x1:
@@ -22,14 +18,35 @@ def draw_line( x0, y0, x1, y1, screen, color):
                 d += 2 * B
             x += 1
             d += 2 * A
-    # FINISH THIS BELOW AT HOME!!!!!
-    # if (A > B):
-    #     B *= -1
-    #     d = 2 * B + A
-    #     while y <= y1:
-    #         plot(screen, color, x,y)
-    #         if d < 0:
-    #             x += 1
-    #             d += B
-    #         x += 1
-    #         d += A
+    if (A > B):
+        B *= -1
+        d = 2 * B + A
+        while y <= y1:
+            plot(screen, color, x, y)
+            if d < 0:
+                x += 1
+                d += 2 * A
+            y += 1
+            d += 2 * B
+    if (A >= -1 * B):
+        B *= -1
+        d = A - 2 * B
+        while y >= y1:
+            plot(screen, color, x, y)
+            if d > 0:
+                x += 1
+                d += 2 * A
+            y -= 1
+            d -= 2 * B
+    if (A > 0 and A < -1 * B):
+        print("hello")
+        print(x0,y0)
+        B *= -1
+        d = 2 * A - B
+        while x <= x1:
+            plot(screen, color, x, y)
+            if d > 0:
+                y -= 1
+                d -= 2 * B
+            x += 1
+            d += 2 * A
--- main.py
+++ main.py
@@ -10,28 +10,28 @@ draw_line(0, 0, XRES-1, YRES / 2, s, c)
 draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
 
 #octants 8 and 4
-# c[BLUE] = 255;
-# draw_line(0, YRES-1, XRES-1, 0, s, c);
-# draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
-# draw_line(XRES-1, 0, 0, YRES/2, s, c);
+c[BLUE] = 255;
+draw_line(0, YRES-1, XRES-1, 0, s, c);
+draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+draw_line(XRES-1, 0, 0, YRES/2, s, c);
 
 #octants 2 and 6
-# c[RED] = 255;
-# c[GREEN] = 0;
-# c[BLUE] = 0;
-# draw_line(0, 0, XRES/2, YRES-1, s, c);
-# draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+c[RED] = 255;
+c[GREEN] = 0;
+c[BLUE] = 0;
+draw_line(0, 0, XRES/2, YRES-1, s, c);
+draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
 
 #octants 7 and 3
-# c[BLUE] = 255;
-# draw_line(0, YRES-1, XRES/2, 0, s, c);
-# draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+c[BLUE] = 255;
+draw_line(0, YRES-1, XRES/2, 0, s, c);
+draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
 
 #horizontal and vertical
-# c[BLUE] = 0;
-# c[GREEN] = 255;
-# draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
-# draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+c[BLUE] = 0;
+c[GREEN] = 255;
+draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
 
 
 display(s)
